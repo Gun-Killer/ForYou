71bcea2b19ebec0fbbfd734e7845f02c2786d989
gunkiller <muse520@outlook.com>
2019-08-31 20:26:54 +0800

    add new sln add new csproj

---
+++ src/ForBasis/DataStructure/List.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructure
+{
+    /// <summary>
+    /// list
+    /// </summary>
+    public class List
+    {
+        
+    }
+}
