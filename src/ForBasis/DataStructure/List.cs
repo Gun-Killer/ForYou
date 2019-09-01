e220c785761ef07cd793cfe3cf411cfe85babdd7
gunkiller <muse520@outlook.com>
2019-09-01 11:50:03 +0800

    add remove method

--- src/ForBasis/DataStructure/List.cs
+++ src/ForBasis/DataStructure/List.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace DataStructure
 {
@@ -10,23 +11,17 @@ namespace DataStructure
         private int _capacity;
         private int _size;
         private T[] _buffer;
+        private IEqualityComparer<T> _equalityComparer;
 
         /// <summary>
         /// 容量
         /// </summary>
-        public int Capacity
-        {
-            get => _capacity;
-            //set => _capacity = value;
-        }
+        public int Capacity => _capacity;
 
         /// <summary>
         /// 实际数量
         /// </summary>
-        public int Count
-        {
-            get => _size;
-        }
+        public int Count => _size;
 
 
         public List()
@@ -34,6 +29,7 @@ namespace DataStructure
             _capacity = 5;
             _size = 0;
             _buffer = new T[_capacity];
+            _equalityComparer = EqualityComparer<T>.Default;
         }
 
         /// <summary>
@@ -54,6 +50,73 @@ namespace DataStructure
             }
             _buffer[_size] = value;
             _size++;
+        }
+
+        /// <summary>
+        /// 移除一个元素
+        /// </summary>
+        /// <param name="value"></param>
+        public void Remove(T value)
+        {
+            var index = GetIndex(value);
+            RemoveAt(index);
+        }
+
+        /// <summary>
+        /// 移除指定位置元素
+        /// </summary>
+        /// <param name="index"></param>
+        public void RemoveAt(int index)
+        {
+            if (index >= _size)
+            {
+                throw new IndexOutOfRangeException("index out");
+            }
+
+            var lastIndex = _size - 1;
+            for (int i = index; i < _size; i++)
+            {
+                if (i >= lastIndex)
+                {
+                    _buffer[i] = default(T);
+                }
+                else
+                {
+                    _buffer[i] = _buffer[i + 1];
+                }
+            }
+
+            _size--;
+        }
+
+        private int GetIndex(T value)
+        {
+            if (_size < 1)
+            {
+                throw new ArgumentOutOfRangeException(nameof(value), "not found value");
+            }
+
+            if (value is IEquatable<T> equatable)
+            {
+                for (int i = 0; i < _size; i++)
+                {
+                    if (equatable.Equals(_buffer[i]))
+                    {
+                        return i;
+                    }
+                }
+            }
+            else
+            {
+                for (int i = 0; i < _size; i++)
+                {
+                    if (_equalityComparer.Equals(value, _buffer[i]))
+                    {
+                        return i;
+                    }
+                }
+            }
+            throw new ArgumentOutOfRangeException(nameof(value), "not found value");
         }
     }
 }
