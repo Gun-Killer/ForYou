a5c4408001317d805e9098127d041c38dc2a33bd
gunkiller <muse520@outlook.com>
2019-09-20 22:37:40 +0800

    添加SHA512SignService

---
+++ src/ForMemory/ForMemory.Service/Sign/SHA512SignService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+using ForMemory.Domain.Interfaces.Services.Sign;
+
+namespace ForMemory.Service.Sign
+{
+    public class SHA512SignService : ISignService
+    {
+        /// <inheritdoc />
+        public string SignType { get; }
+
+        public SHA512SignService()
+        {
+            SignType = "SHA512";
+        }
+
+        /// <inheritdoc />
+        public bool Next(string signType)
+        {
+            return !SignType.Equals(signType, StringComparison.InvariantCultureIgnoreCase);
+        }
+
+        /// <inheritdoc />
+        public string Sign(string value)
+        {
+            if (string.IsNullOrEmpty(value)) throw new ArgumentNullException(nameof(value));
+            
+            using var cryptoServiceProvider = new SHA512CryptoServiceProvider();
+            var bytes = Encoding.UTF8.GetBytes(value);
+            var hash = cryptoServiceProvider.ComputeHash(bytes);
+            return Convert.ToBase64String(hash);
+        }
+    }
+}
