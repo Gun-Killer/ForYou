8d5719a165696b8527580d7b24a56379e732dce7
gunkiller <muse520@outlook.com>
2019-09-22 21:20:59 +0800

    添加实体

---
+++ src/ForMemory/ForMemory.Entities/Accounting/AccountEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using ForMemory.Entities.Interfaces;
+
+namespace ForMemory.Entities.Accounting
+{
+    public class AccountEntity : IEntity
+    {
+        /// <inheritdoc />
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal Amount { get; set; }
+    }
+}
---
+++ src/ForMemory/ForMemory.Entities/Accounting/ExpensesRecordEntity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using ForMemory.Entities.Interfaces;
+
+namespace ForMemory.Entities.Accounting
+{
+    public class ExpensesRecordEntity : IEntity
+    {
+        /// <inheritdoc />
+        public Guid Id { get; set; }
+
+        public Guid AccountId { get; set; }
+
+        public Guid FamilyId { get; set; }
+
+        public Guid FamilyMemberId { get; set; }
+
+        public DateTime Time { get; set; }
+
+        public decimal Amount { get; set; }
+
+        public int Type { get; set; }
+
+        public string Remark { get; set; }
+    }
+}
