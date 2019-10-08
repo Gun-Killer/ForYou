018c0e48a2d52ed58ed5a10941497b00ba8a3d12
gunkiller <muse520@outlook.com>
2019-10-08 21:53:16 +0800

    定义结构

---
+++ src/ForMemory/ForMemory.Cache/CachePool.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace ForMemory.Cache
+{
+    public class CachePool<T>
+    {
+        private readonly List<T> _pools;
+
+        public CachePool()
+        {
+            _pools = new List<T>(5);
+        }
+    }
+}
---
+++ src/ForMemory/ForMemory.Cache/RedisCache.cs
@@ -0,0 +1,19 @@
+﻿using ForMemory.Cache.Interfaces;
+
+namespace ForMemory.Cache
+{
+    public class RedisCache : ICache
+    {
+        /// <inheritdoc />
+        public string Get(string key)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public bool Set(string key, string value, int minutes)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
