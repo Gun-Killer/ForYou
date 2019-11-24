bf012b4f6055ae1920b9bbc75bfec93ed91a14c5
gunkiller <muse520@outlook.com>
2019-11-24 11:02:45 +0800

    Sample

---
+++ src/ZooKeeper/ZooKeeper.ClientSample/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using org.apache.zookeeper;
+
+namespace ZooKeeper.ClientSample
+{
+    internal class Program
+    {
+        private static async Task Main(string[] args)
+        {
+
+            org.apache.zookeeper.ZooKeeper zooKeeper = new org.apache.zookeeper.ZooKeeper("127.0.0.1:2181", 5000, new WatcherSample());
+
+            //创建个节点，数据为1111
+            var res = await zooKeeper.createAsync("/root", Encoding.UTF8.GetBytes("11111"), ZooDefs.Ids.OPEN_ACL_UNSAFE,
+                CreateMode.EPHEMERAL);
+            Console.WriteLine(res);
+
+            //获取节点数据
+            var midRes = await zooKeeper.getDataAsync("/root");
+            Console.WriteLine(Encoding.UTF8.GetString(midRes.Data));
+
+            await Task.CompletedTask;
+        }
+    }
+}
---
+++ src/ZooKeeper/ZooKeeper.ClientSample/WatcherSample.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+using org.apache.zookeeper;
+
+namespace ZooKeeper.ClientSample
+{
+    internal class WatcherSample : Watcher
+    {
+        /// <inheritdoc />
+        public override Task process(WatchedEvent @event)
+        {
+            Console.WriteLine(@event.getState());
+            return Task.CompletedTask;
+        }
+    }
+}
