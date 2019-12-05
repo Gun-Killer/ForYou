0350f769de8e16f058b9435f6ee79c68aa4a1ba5
gunkiller <muse520@outlook.com>
2019-12-05 21:43:17 +0800

    string  order by zn-CN mark

---
+++ src/ZooKeeper/ZooKeeper.ClientSample/OrderByZhCn.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace ZooKeeper.ClientSample
+{
+    public static class OrderByZhCn
+    {
+        public static IEnumerable<T> OrderByZnCh<T>(this IEnumerable<T> source, Func<T, string> keySelect)
+        {
+            CultureInfo culture = new CultureInfo("zn-CN");
+            return source.OrderBy(keySelect, StringComparer.Create(culture, false));
+        }
+    }
+}
