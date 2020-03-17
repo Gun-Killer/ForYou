ea4cd5b908e29c40592e87ce826cf14fcca0489b
gunkiller <muse520@outlook.com>
2020-03-17 22:13:00 +0800

    通用消息格式

---
+++ src/ForIM/IM/IM/Services/Infrastructure/DefaultSocketMessage.cs
@@ -0,0 +1,18 @@
+﻿namespace ForYou.ForIM.Services.Infrastructure
+{
+    /// <inheritdoc />
+    public class DefaultSocketMessage : IMessage
+    {
+        /// <inheritdoc />
+        public string Id { get; set; }
+
+        /// <inheritdoc />
+        public MessageType Type { get; set; }
+
+        /// <inheritdoc />
+        public string Content { get; set; }
+
+        /// <inheritdoc />
+        public string MediaId { get; set; }
+    }
+}
---
+++ src/ForIM/IM/IM/Services/Infrastructure/IMessage.cs
@@ -0,0 +1,43 @@
+﻿namespace ForYou.ForIM.Services.Infrastructure
+{
+    /// <summary>
+    /// 消息格式
+    /// </summary>
+    public interface IMessage
+    {
+
+        /// <summary>
+        /// 消息唯一标识
+        /// </summary>
+        string Id { get; set; }
+        /// <summary>
+        /// 类型
+        /// </summary>
+        MessageType Type { get; set; }
+
+        /// <summary>
+        /// 文本内容
+        /// </summary>
+        string Content { get; set; }
+
+        /// <summary>
+        /// 媒体Id
+        /// </summary>
+        string MediaId { get; set; }
+    }
+
+    /// <summary>
+    /// 消息类型
+    /// </summary>
+    public enum MessageType
+    {
+        /// <summary>
+        /// 文本
+        /// </summary>
+        Text = 1,
+        /// <summary>
+        /// 图文
+        /// </summary>
+        Image = 2
+    }
+}
