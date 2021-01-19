3c22927b58c82fe10f96660a54f5dcf21744f7eb
muse520@outlook.com <muse520@outlook.com>
2021-01-19 20:18:03 +0800

    feat: modify cache interface

--- src/ForMemory/Cache/ForMemory.Cache/Interfaces/ICache.cs
+++ src/ForMemory/Cache/ForMemory.Cache/Interfaces/ICache.cs
@@ -1,9 +1,26 @@
 ﻿namespace ForMemory.Cache.Interfaces
 {
+    /// <summary>
+    /// 缓存通用接口
+    /// </summary>
     public interface ICache
     {
-        string Get(string key);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        T Get<T>(string key);
 
-        bool Set(string key, string value, int minutes);
+        /// <summary>
+        /// /
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        /// <param name="minutes"></param>
+        /// <returns></returns>
+        bool Set<T>(string key, T value, int minutes);
     }
 }
