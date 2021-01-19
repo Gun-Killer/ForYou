042a7d03f63e5cc50fb2e82d5c76a0c1ad798bc8
muse520@outlook.com <muse520@outlook.com>
2021-01-19 20:19:56 +0800

    feat:add doc

--- src/ForMemory/Cache/ForMemory.Cache/Interfaces/ICache.cs
+++ src/ForMemory/Cache/ForMemory.Cache/Interfaces/ICache.cs
@@ -6,20 +6,20 @@
     public interface ICache
     {
         /// <summary>
-        /// 
+        /// 获取key 对应的缓存数据
         /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="key"></param>
+        /// <typeparam name="T">类型</typeparam>
+        /// <param name="key">缓存key</param>
         /// <returns></returns>
         T Get<T>(string key);
 
         /// <summary>
-        /// /
+        /// 设置缓存
         /// </summary>
         /// <typeparam name="T"></typeparam>
-        /// <param name="key"></param>
-        /// <param name="value"></param>
-        /// <param name="minutes"></param>
+        /// <param name="key">缓存标识</param>
+        /// <param name="value">缓存数据</param>
+        /// <param name="minutes">缓存时间 单位分钟</param>
         /// <returns></returns>
         bool Set<T>(string key, T value, int minutes);
     }
