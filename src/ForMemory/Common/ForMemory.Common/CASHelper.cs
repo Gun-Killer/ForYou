1033b31aba8cb4d18f981ed08d8c21f5f1d93e48
muse520@outlook.com <muse520@outlook.com>
2021-03-16 22:34:31 +0800

    feat:cas 使用

---
+++ src/ForMemory/Common/ForMemory.Common/CASHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+
+namespace ForMemory.Common
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class CASHelper
+    {
+        public static void CAS<T>(ref T input, Func<T, T> func) where T : class
+        {
+            T temp, replace;
+            do
+            {
+                temp = input;
+                replace = func(temp);
+            } while (Interlocked.CompareExchange(ref input, replace, temp) != temp);
+        }
+    }
+}
---
+++ src/ForMemory/Common/ForMemory.Common/SpinLock.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading;
+
+namespace ForMemory.Common
+{
+    public class SpinLock
+    {
+        private volatile int _lock;
+
+        public void Lock()
+        {
+            while (Interlocked.CompareExchange(ref _lock, 1, 0) != 0)
+            {
+                ;
+            }
+        }
+
+        public void Release()
+        {
+            _lock = 0;
+        }
+    }
+}
